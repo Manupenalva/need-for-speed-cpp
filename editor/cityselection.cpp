11800a0f2523f53b89181e56e61d648bbac5c6bf
FacuMadotta <fmadotta@fi.uba.ar>
2025-10-30 23:13:53 -0300

    agrego nombre a las imagenes

--- editor/cityselection.cpp
+++ editor/cityselection.cpp
@@ -30,13 +30,19 @@ CitySelection::CitySelection(QWidget* parent): QWidget(parent) {
                           "Backgrounds - Vice City.png"}};
 
     for (auto& city: cities) {
+        QVBoxLayout* cityLayout = new QVBoxLayout();
         QPushButton* cityButton = new QPushButton();
         cityButton->setIcon(QIcon(city.iconPath));
         cityButton->setIconSize(QSize(200, 200));
-        cityButton->setFixedSize(220, 250);
+        cityButton->setFixedSize(220, 220);
         QObject::connect(cityButton, &QPushButton::clicked, this,
-                         [this, city]() { emit citySelected(city.iconPath); });
-        citiesLayout->addWidget(cityButton);
+                        [this, city]() { emit citySelected(city.iconPath); });
+        QLabel* nameLabel = new QLabel(city.name);
+        nameLabel->setAlignment(Qt::AlignCenter);
+        nameLabel->setStyleSheet("font-weight: bold; font-size: 14px;");
+        cityLayout->addWidget(cityButton);
+        cityLayout->addWidget(nameLabel);
+        citiesLayout->addLayout(cityLayout);
     }
 
     mainLayout->addLayout(citiesLayout);
